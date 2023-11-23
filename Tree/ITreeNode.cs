4cd39912da4beddeb3ed36290af628b499cc5a45
kbr <kbr@REOKOBAYASHI>
2023-11-23 22:16:41 +0900

    fix

--- Tree/ITreeNode.cs
+++ Tree/ITreeNode.cs
@@ -3,15 +3,25 @@
     public interface ITreeNode<T>
     {
         T Parent { get; set; }
+
         IList<T> Children { get; set; }
+
         T FindNode(string name);
+
         void DisplayTree(int depth = 0);
+
         string ToString(int depth = 0);
+
         T AddChild(T child);
+
         T RemoveChild(T child);
+
         bool TryRemoveChild(T child);
+
         T ClearChildren();
+
         bool TryRemoveOwn();
+
         T RemoveOwn();
     }
 }
